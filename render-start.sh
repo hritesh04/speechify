3cf1419c17619c865beb55727edd29ccc7766ed6
hritesh04 <hritesh04.dev@gmail.com>
2024-04-26 23:42:47 +0530

    chrome added

--- render-start.sh
+++ render-start.sh
@@ -4,22 +4,21 @@ CHROME_URL="https://dl.google.com/linux/direct/google-chrome-stable_current_amd6
 
 INSTALL_DIR="/opt/google/chrome"
 
-echo "Downloading Google Chrome..."
-wget -q --show-progress -O /tmp/google-chrome.deb $CHROME_URL
+STORAGE_DIR=/opt/render/project/.render
 
-echo "Installing Google Chrome..."
-dpkg -i /tmp/google-chrome.deb
-
-if [ -x "$(command -v google-chrome)" ]; then
-    echo "Google Chrome installed successfully!"
-    echo "Adding Chrome to PATH..."
-    echo "export PATH=\$PATH:$INSTALL_DIR" >> ~/.bashrc
-    source ~/.bashrc
-    echo "Chrome added to PATH."
+if [[ ! -d $STORAGE_DIR/chrome ]]; then
+echo "...Downloading Chrome"
+mkdir -p $STORAGE_DIR/chrome
+cd $STORAGE_DIR/chrome
+wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+rm ./google-chrome-stable_current_amd64.deb
+cd $HOME/project/src # Make sure we return to where we were
 else
-    echo "Failed to install Google Chrome."
+echo "...Using Chrome from cache"
 fi
 
-rm /tmp/google-chrome.deb
-
-echo "Installation completed."
+# be sure to add Chromes location to the PATH as part of your Start Command
+# export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome/"
+# echo $STORAGE_DIR
+# echo $PATH
