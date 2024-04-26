247703a02f31dd7c2920635c567374b3cddfcd35
hritesh04 <hritesh04.dev@gmail.com>
2024-04-27 00:01:32 +0530

    chrome added

--- render-start.sh
+++ render-start.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
 CHROME_URL="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-INSTALL_DIR="/opt/google/chrome"
+INSTALL_DIR="/opt/google"
 
 # Ensure the installation directory exists
-mkdir -p $INSTALL_DIR
+mkdir -p $INSTALL_DIR/chrome
 
 # Download Google Chrome
-echo "Downloading Google Chrome..."
-wget -q --show-progress -O /tmp/google-chrome.deb $CHROME_URL
+if [[ -d $INSTALL_DIR/chrome ]]; then
+    echo "Downloading Google Chrome..."
 
-# Install Google Chrome (assuming dpkg doesn't require sudo)
-echo "Installing Google Chrome..."
-dpkg -i /tmp/google-chrome.deb
+    cd $INSTALL_DIR/chrome
+    wget -P ./ $CHROME_URL
 
-# Verify installation and set Chrome to PATH
-if [ -x "$(command -v google-chrome)" ]; then
+    echo "Installing Google Chrome..."
+    dpkg -x ./google-chrome-stable_current_amd64.deb $INSTALL_DIR/chrome
+
+    rm ./google-chrome-stable_current_amd64.deb
+    cd $HOME
     echo "Google Chrome installed successfully!"
     echo "Adding Chrome to PATH..."
     echo "export PATH=\$PATH:$INSTALL_DIR" >> $HOME/.bashrc
     source $HOME/.bashrc
     echo "Chrome added to PATH."
 else
-    echo "Failed to install Google Chrome."
+    echo "Using Chrome from cache"
 fi
-
-# Clean up downloaded package
-rm /tmp/google-chrome.deb
 
 echo "Installation completed."
