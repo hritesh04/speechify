22d6bf42a9493825503e288552d55a2712c51c5c
hritesh04 <hritesh04.dev@gmail.com>
2024-04-27 00:14:40 +0530

    chrome added

--- .render/render-start.sh
+++ .render/render-start.sh
@@ -3,25 +3,24 @@
 CHROME_URL="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 INSTALL_DIR=".render/google"
 
-# Ensure the installation directory exists
-mkdir -p $INSTALL_DIR/chrome
+mkdir -p "$INSTALL_DIR/chrome"
 
 # Download Google Chrome
-if [[ -d $INSTALL_DIR/chrome ]]; then
+if [[ -d "$INSTALL_DIR/chrome" ]]; then
     echo "Downloading Google Chrome..."
 
-    cd $INSTALL_DIR/chrome
-    wget -P ./ $CHROME_URL
+    cd "$INSTALL_DIR/chrome"
+    wget -P ./ "$CHROME_URL"
 
     echo "Installing Google Chrome..."
-    dpkg -x ./google-chrome-stable_current_amd64.deb $INSTALL_DIR/chrome
+    dpkg -x ./google-chrome-stable_current_amd64.deb "$INSTALL_DIR/chrome"
 
     rm ./google-chrome-stable_current_amd64.deb
-    cd $HOME
+    cd "$HOME"
     echo "Google Chrome installed successfully!"
     echo "Adding Chrome to PATH..."
-    echo "export PATH=\$PATH:$INSTALL_DIR" >> $HOME/.bashrc
-    source $HOME/.bashrc
+    echo "export PATH=\$PATH:$INSTALL_DIR/chrome" >> "$HOME/.bashrc"
+    source "$HOME/.bashrc"
     echo "Chrome added to PATH."
 else
     echo "Using Chrome from cache"
